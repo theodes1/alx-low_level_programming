c3f5e4ce732db2cbb095f3e12259866c7097266f
theodes1 <obasam67@gmail.com>
2022-09-08 11:56:46 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char c;
@@ -12,10 +13,12 @@ int main(void)
 	long l;
 	long long ll;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof (c));
 	printf("Size of an int: %lu byte(s)\n", sizeof (i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof (l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof (ll));
 	printf("Size of a float: %lu byte(s)\n", sizeof (f));
+
 	return (0);
 }
