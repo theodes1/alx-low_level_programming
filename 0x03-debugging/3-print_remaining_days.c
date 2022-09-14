a89a3f6643497dd5b5cc5938b71e0b95318665c8
theodes1 <obasam67@gmail.com>
2022-09-14 20:34:52 +0100

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,6 +9,7 @@
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
 {
 
@@ -27,7 +28,8 @@ void print_remaining_days(int month, int day, int year)
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
+		} vi 3-print_remaining_days.c
+
 		else
 		{
 			printf("Day of the year: %d\n", day);
