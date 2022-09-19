26d4cccd9e4aa0b630c787195812ea515f48f3d6
theodes1 <obasam67@gmail.com>
2022-09-19 05:35:11 -0700

    task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-* reset_to_98 -a function that takes a pointer to an int
-* @n: puntero
-* Return: (0)
-*/
+ * reset_to_98 - check the code
+ * @n: pointer to a variable.
+ */
 void reset_to_98(int *n)
 {
 	int *p = n;
