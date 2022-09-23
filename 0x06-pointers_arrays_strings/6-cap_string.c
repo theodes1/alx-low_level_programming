ee214b0cb9585e145d32f21c1e78173ec0a7d119
theodes1 <obasam67@gmail.com>
2022-09-23 20:13:02 +0100

    updated

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *cap_string - check the code
+ * @s: First variable.
+ * Return: all words capitalized.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+				s[i] = s[i] - 32;
+			continue;
+		}
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' ||
+			s[i] == ':' || s[i] == '.' || s[i] == '?' || s[i] == '!' ||
+			s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+			s[i] == '}')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *leet - check the code
+ * @s:  Pointer to a
+ * Return: return s.
+ */
+char *leet(char *s)
+{
+	int i = 0;
+	int n = 0;
+
+	char let[] = "AaEeOoTtLl";
+	char num[] = "4433007711";
+
+	while (s[i] != '\0')
+	{
+		n = 0;
+		while (let[n] != '\0')
+		{
+			if (s[i] == let[n])
+			{
+				s[i] = num[n];
+			}
+			n++;
+		}
+		i++;
+	}
+	return (s);
+}
