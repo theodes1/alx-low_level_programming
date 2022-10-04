ce37be58e1d356c66fd18af51bdeb67722f7f159
theodes1 <obasam67@gmail.com>
2022-10-04 05:18:06 +0100

    task

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,23 +8,27 @@
  */
 char *_strdup(char *str);
 {
-	char *strDup;
-	int i, j;
+	char *str2;
+	int n = 0;
+	int x;
 
 	if (str == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	i++;
-	strDup = malloc(sizeof(*str) * i);
-	if (strDup == NULL)
-		return (NULL);
-	j = 0;
-	while (str[j] != '\0')
 	{
-		strDup[j] = str[j];
-		j++;
+		return (NULL);
 	}
-	return (strDup);
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+	n++;
+	str2 = malloc(n * sizeof(char));
+	if (str2 == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < n; x++)
+	{
+		str2[x] = str[x];
+	}
+	return (str2);
 }
