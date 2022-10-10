788aa0bb1e9be96e2d360785310ddb8ab1f7a08b
theodes1 <obasam67@gmail.com>
2022-10-10 19:57:30 +0100

    task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file
+ * Return: void
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+fndef ABS_H
+#define ABS_H
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+#endif
