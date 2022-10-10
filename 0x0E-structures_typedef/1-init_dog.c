9c668cd6b247fd8be714de89962d9ef6a08c0243
theodes1 <obasam67@gmail.com>
2022-10-10 19:43:23 +0100

    tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+ * init_dog - initialize dog
+ * @d: pointer variable
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to print dog data
+ * @d: pointer variable
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
