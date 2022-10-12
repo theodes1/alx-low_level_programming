13b0fd789cf116c00f018050dfd71913d800ad3b
theodes1 <obasam67@gmail.com>
2022-10-12 16:09:30 +0100

    task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - calculator
+ * @argc: number of arguments
+ * @argv: array of pointers to arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((argv[2][0] != 37 && argv[2][0] != 47 && argv[2][0] != 43 &&
+	argv[2][0] != 45 && argv[2][0] != 42) || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
