4a85bd854703b7d0029864ea8131f91845f7c31b
theodes1 <obasam67@gmail.com>
2022-10-13 20:33:48 +0100

    r

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,14 +7,17 @@
  * @print: The operator
  * @f: The function associated
  */
-typedef struct print
+
+typedef struct printer
 {
-	char *print;
-	void (*f)(va_list args);
-} print_t
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printer_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif
