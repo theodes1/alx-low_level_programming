84727178f911a2112e8a4c5bb452b2e1bcd97905
theodes1 <obasam67@gmail.com>
2022-10-27 15:56:56 +0100

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * Function check_for_endianness
- * returns 1, if architecture is little endian
- * return 0, if architecture is big endian
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
  */
 int get_endianness(void)
 {
