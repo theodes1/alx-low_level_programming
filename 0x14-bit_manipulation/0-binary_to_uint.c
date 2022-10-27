6269c5e16050b4ff93d35119a719641cb45c979b
theodes1 <obasam67@gmail.com>
2022-10-27 14:20:00 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 	 unsigned int val = 0;
+	 unsigned int aux = 1;
 	 int i = 0;
 
 	if (b == NULL)
 	{
-		return 0;
+		return (0);
 	}
-
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-		val <<= 1;
-		val += b[i]-'0';
 		i++;
+	}
+	for (i = (i - 1); i >= 0; i--)
+	{
+		if (b[i] != 48 && b[i] != 49)
+		{
+			return (0);
+		}
+		val = val + ((b[i] - 48) * aux);
+		aux = aux * 2;
 	}
 	return (val);
 }
