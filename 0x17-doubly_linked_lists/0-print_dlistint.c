b917b53fac27f0ba2c45d57a3991ac4cf218b31c
theodes1 <obasam67@gmail.com>
2022-12-08 20:12:05 +0100

    function that prints all the elements of a dlistint_t list.

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,30 +1,22 @@
 #include "lists.h"
-
 /**
- * print_dlistint - prints all the elements of a
- * dlistint_t list
- *
- * @h: head of the list
- * Return: the number of nodes
+ * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * @h: Head of the linked list.
+ * Return: the number of nodes.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
+	size_t count = 0;
+	const dlistint_t *i;
 
 	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+		return (0);
+	i = h;
+	while (i != NULL)
 	{
-		printf("%d\n", h->n);
 		count++;
-		h = h->next;
+		printf("%d\n", i->n);
+		i = i->next;
 	}
-
 	return (count);
 }
